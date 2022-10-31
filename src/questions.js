27025d2ef0d9f0bb23b0227ee1ac61cf6b8978e7
jesterb0206 <jesterb@seattleu.edu>
2022-10-31 14:11:37 -0700

    Intern Questions

--- src/questions.js
+++ src/questions.js
@@ -46,7 +46,7 @@ const managerQuestions = [
   },
 ];
 
-const engineerQuestion = [
+const engineerQuestions = [
   {
     type: "input",
     message: "what's the engineer's name?",
@@ -91,3 +91,61 @@ const engineerQuestion = [
     },
   },
 ];
+
+const internQuestions = [
+  {
+    type: "input",
+    message: "What's the intern's name?",
+    name: "name",
+    validate: function (input) {
+      return typeof input === "string"
+        ? true
+        : "Please enter the intern's name!";
+    },
+  },
+
+  {
+    type: "input",
+    message: "What's the intern's employee ID?",
+    name: "id",
+    validate: function (id) {
+      return isNaN(parseInt(id))
+        ? "Please enter the intern's employee ID!"
+        : true;
+    },
+  },
+
+  {
+    type: "input",
+    message: "What's the intern's email address?",
+    name: "email",
+    validate: function (email) {
+      return (
+        /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+          email
+        ) || "Please enter the intern's email!"
+      );
+    },
+  },
+
+  {
+    type: "input",
+    message: "What school does the intern attend?",
+    name: "school",
+    validate: function (school) {
+      return !!school || "Please enter the intern's school's name!";
+    },
+  },
+];
+
+const menu = [
+  {
+    type: "list",
+    name: "choice",
+    message:
+      "Add an engineer or intern to your team or select finished if all done!",
+    choices: ["Engineer", "Intern", "Finished"],
+  },
+];
+
+module.exports = { managerQuestions, engineerQuestions, internQuestions, menu };
